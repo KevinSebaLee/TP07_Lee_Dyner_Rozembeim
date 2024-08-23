64039e1b872bedce18e27328c3a5e114a182bcc6
KevinSebaLee <48044115@est.ort.edu.ar>
2024-08-23 12:58:03 -0300

    Empezando el ConfigurarJuego y creacion de Fin.cshtml

--- Preguntados.sql
+++ Preguntados.sql
@@ -2,31 +2,30 @@ CREATE DATABASE PREGUNTADOS;
 
 USE PREGUNTADOS;
 
-CREATE TABLE Preguntas{
+CREATE TABLE Categorias(
+    IdCategoria INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    Nombre VARCHAR(200) NOT NULL,
+    Foto TEXT NOT NULL
+);
+
+CREATE TABLE Dificultades(
+    IdDificultad INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    Nombre VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Preguntas(
     IdPregunta INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
     IdCategoria INT NOT NULL FOREIGN KEY REFERENCES Categorias(IdCategoria),
     IdDificultad INT NOT NULL FOREIGN KEY REFERENCES Dificultades(IdDificultad),
     Enunciado VARCHAR(200) NOT NULL,
     Foto TEXT NOT NULL
-}
+);
 
-CREATE TABLE Categorias{
-    IdCategoria INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-    Nombre VARCHAR(200) NOT NULL,
-    Foto TEXT NOT NULL
-}
-
-CREATE TABLE Respuestas{
+CREATE TABLE Respuestas(
     IdRespuesta INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
     IdPregunta INT NOT NULL FOREIGN KEY REFERENCES Preguntas(IdPregunta),
     Opcion INT NOT NULL,
     Contenido VARCHAR(200) NOT NULL,
     Correcta BIT NOT NULL,
     Foto TEXT NOT NULL
-}
-
-CREATE TABLE Dificultades{
-    IdDificultad INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-    Nombre VARCHAR(200) NOT NULL
-}
-
+);
