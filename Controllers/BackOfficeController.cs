f4ac517ffb95a5789d235d8d097627e653a03c2e
KevinSebaLee <48044115@est.ort.edu.ar>
2024-09-16 08:29:46 -0300

    Finalizazr stilizacion de los botones de correcto o incorrecto

--- Controllers/BackOfficeController.cs
+++ Controllers/BackOfficeController.cs
@@ -6,6 +6,12 @@ namespace Preguntados.Controllers;
 
 public class BackOfficeController : Controller
 {
+    private readonly ILogger<BackOfficeController> _logger;
+
+    public BackOfficeController(ILogger<BackOfficeController> logger)
+    {
+        _logger = logger;
+    }
     public IActionResult CrearPregunta(int idCategoria, int idDificultad, string Enunciado, string Respuesta1, string Respuesta2, string Respuesta3, string Respuesta4, int Opcion1, int Opcion2, int Opcion3, int Opcion4, int Correcta){
         Juego.CrearPreguntas(idCategoria, idDificultad, Enunciado, Respuesta1, Respuesta2, Respuesta3, Respuesta4, Opcion1, Opcion2, Opcion3, Opcion4, Correcta);
 
